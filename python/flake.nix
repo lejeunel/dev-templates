fa31ceedc3aeb7ac3e3bb27bf51a99774be6f103
lejeunel <me@lejeunel.org>
2024-08-21 12:53:34 +0200

    python poetry lsp

--- python/flake.nix
+++ python/flake.nix
@@ -5,19 +5,21 @@
 
   outputs = { self, nixpkgs }:
     let
-      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
-        pkgs = import nixpkgs { inherit system; };
-      });
-    in
-    {
+      supportedSystems =
+        [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f:
+        nixpkgs.lib.genAttrs supportedSystems
+        (system: f { pkgs = import nixpkgs { inherit system; }; });
+    in {
       devShells = forEachSupportedSystem ({ pkgs }: {
         default = pkgs.mkShell {
           venvDir = ".venv";
-          packages = with pkgs; [ python311 ] ++
-            (with pkgs.python311Packages; [
-              pip
-              venvShellHook
+          packages = with pkgs;
+            [ python311 poetry ] ++ (with pkgs.python311Packages; [
+              python-lsp-server
+              black
+              isort
+              pyflakes
             ]);
         };
       });
